943574f07fd907c7e5afddf8c27aa63f307b3cbc
Shane <6071159+smashedr@users.noreply.github.com>
2023-12-26 23:08:53 -0800

    Add Popup Toast and Cleanup

--- src/js/exports.js
+++ src/js/exports.js
@@ -8,20 +8,19 @@
  * @param {Function} callback
  */
 export function patchRom(url, key, callback) {
-    const smwcWorld = 'https://smwc.world/patcher/'
+    console.debug(`patchRom: ${key}: ${url}`)
+    const patcherUrl = 'https://smwc.world/patcher/'
     const sourceRom =
         'https://github.com/videofindersTV/super-mario-world/raw/master/Super.Mario.World.1.smc'
     const formdata = new FormData()
     formdata.append('patch_url', url)
     formdata.append('source_url', sourceRom)
-
     const requestOptions = {
         method: 'POST',
         body: formdata,
         redirect: 'follow',
     }
-
-    fetch(smwcWorld, requestOptions)
+    fetch(patcherUrl, requestOptions)
         .then((response) => response.json())
         .then((result) => callback(result, key))
         .catch((error) => console.warn('error', error))
@@ -33,18 +32,20 @@ export function patchRom(url, key, callback) {
  * @param {InputEvent} event
  */
 export async function saveOptions(event) {
-    console.log('saveOptions:', event)
+    console.debug('saveOptions:', event)
     const { options } = await chrome.storage.sync.get(['options'])
     let value
     if (event.target.type === 'checkbox') {
         value = event.target.checked
-    } else if (event.target.type === 'text') {
+    } else {
         value = event.target.value
     }
     if (value !== undefined) {
         options[event.target.id] = value
-        console.log(`Set: ${event.target.id}:`, value)
+        console.info(`Set: ${event.target.id}:`, value)
         await chrome.storage.sync.set({ options })
+    } else {
+        console.warn(`Unknown Options ID: ${event.target.id}`)
     }
 }
 
@@ -64,5 +65,28 @@ export function updateOptions(options) {
                 el.value = value
             }
         }
+    }
+}
+
+/**
+ * Show Bootstrap Toast
+ * @function showToast
+ * @param {String} message
+ * @param {String} type
+ */
+export function showToast(message, type = 'success') {
+    console.debug(`showToast: ${type}: ${message}`)
+    const clone = document.querySelector('.d-none .toast')
+    const container = document.getElementById('toast-container')
+    if (clone && container) {
+        const element = clone.cloneNode(true)
+        element.querySelector('.toast-body').innerHTML = message
+        element.classList.add(`text-bg-${type}`)
+        container.appendChild(element)
+        const toast = new bootstrap.Toast(element)
+        element.addEventListener('mousemove', () => toast.hide())
+        toast.show()
+    } else {
+        console.info('Missing clone or container:', clone, container)
     }
 }
